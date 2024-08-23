4f537f09fc3ce4fb2668f18d297fd5beaa700067
MooKorea <69235304+MooKorea@users.noreply.github.com>
2024-08-23 02:14:50 -0500

    rename build folder

--- apps/skraggle/src/app/page.tsx
+++ apps/skraggle/src/app/page.tsx
@@ -3,10 +3,10 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 export default function Home() {
   const { unityProvider } = useUnityContext({
-    loaderUrl: "./game-files/UnityBuild/game-files.loader.js",
-    dataUrl: "./game-files/UnityBuild/game-files.data.br",
-    frameworkUrl: "./game-files/UnityBuild/game-files.framework.js.br",
-    codeUrl: "./game-files/UnityBuild/game-files.wasm.br",
+    loaderUrl: "./game-files/Build/game-files.loader.js",
+    dataUrl: "./game-files/Build/game-files.data.br",
+    frameworkUrl: "./game-files/Build/game-files.framework.js.br",
+    codeUrl: "./game-files/Build/game-files.wasm.br",
   });
 
   return <Unity unityProvider={unityProvider} />;
