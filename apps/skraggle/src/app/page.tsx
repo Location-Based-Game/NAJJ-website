d00afe49b0af99381b32f2828752a4f542929419
MooKorea <69235304+MooKorea@users.noreply.github.com>
2024-08-23 02:08:05 -0500

    use brotli

--- apps/skraggle/src/app/page.tsx
+++ apps/skraggle/src/app/page.tsx
@@ -4,9 +4,9 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 export default function Home() {
   const { unityProvider } = useUnityContext({
     loaderUrl: "./game-files/UnityBuild/game-files.loader.js",
-    dataUrl: "./game-files/UnityBuild/game-files.data.gz",
-    frameworkUrl: "./game-files/UnityBuild/game-files.framework.js.gz",
-    codeUrl: "./game-files/UnityBuild/game-files.wasm.gz",
+    dataUrl: "./game-files/UnityBuild/game-files.data.br",
+    frameworkUrl: "./game-files/UnityBuild/game-files.framework.js.br",
+    codeUrl: "./game-files/UnityBuild/game-files.wasm.br",
   });
 
   return <Unity unityProvider={unityProvider} />;
