b567c51f61394fd1d642f8538c8a1ff3d67e357b
MooKorea <69235304+MooKorea@users.noreply.github.com>
2024-10-19 19:28:58 -0500

    update yml

--- apps/skraggle/environment.d.ts
+++ apps/skraggle/environment.d.ts
@@ -3,5 +3,6 @@ namespace NodeJS {
     NEXT_PUBLIC_PLACEHOLDER_CODE: "jr2p";
     NEXT_PUBLIC_USE_PLACEHOLDER_CODE: "true" | "false";
     JWT_SECRET_TOKEN: string;
+    SKRAGGLE_FIREBASE_ADMIN: string;
   }
 }
--- apps/skraggle/src/lib/firebaseAdmin.ts
+++ apps/skraggle/src/lib/firebaseAdmin.ts
@@ -1,8 +1,8 @@
 import "server-only";
-import serviceAccount from "../../skraggle-2e19f-firebase-adminsdk-ss9c5-9d01ff3f27.json";
 import * as admin from "firebase-admin";
 
 if (!admin.apps.length) {
+  const serviceAccount = process.env.SKRAGGLE_FIREBASE_ADMIN!
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
     databaseURL: "https://skraggle-2e19f-default-rtdb.firebaseio.com",
