86ec9431e086b8cca642b59aeaac635d808f8ce9
MooKorea <69235304+MooKorea@users.noreply.github.com>
2024-10-23 20:07:50 -0500

    timeout peers if not connected

--- apps/skraggle/src/app/_unity-player/useWebRTC.tsx
+++ apps/skraggle/src/app/_unity-player/useWebRTC.tsx
@@ -1,10 +1,11 @@
-import { onValue, ref, set, get } from "firebase/database";
+import { onValue, ref, get } from "firebase/database";
 import { useEffect, useRef } from "react";
 import { rtdb } from "../firebaseConfig";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
 import Peer from "simple-peer";
 import { fetchApi } from "@/lib/fetchApi";
+import useLogOutOnError from "@/hooks/useLogOutOnError";
 
 export type PlayerPeers = {
   [playerId: string]: Peer.Instance;
@@ -14,7 +15,7 @@ export default function useWebRTC() {
   const { gameId, playerId, playerName } = useSelector(
     (state: RootState) => state.logIn,
   );
-
+  const { logOutOnError } = useLogOutOnError(false);
   const playerPeers = useRef<PlayerPeers>({});
 
   useEffect(() => {
@@ -41,7 +42,7 @@ export default function useWebRTC() {
     });
 
     //listen for offers and create a remote peer & answer for each offer
-    onValue(offerRef, (snapshot) => {
+    const offerListener = onValue(offerRef, (snapshot) => {
       if (!snapshot.exists()) return;
       const data = snapshot.val();
       const remotePeer = createPeer(false, data.playerId);
@@ -50,18 +51,25 @@ export default function useWebRTC() {
     });
 
     //listen for answers and assign signals to their respective player ID's host peer
-    onValue(answerRef, (snapshot) => {
+    const answerListener = onValue(answerRef, (snapshot) => {
       if (!snapshot.exists()) return;
       const data = snapshot.val();
       const key = data.playerId;
       playerPeers.current[key].signal(data.signal);
     });
+
+    //unsubscribe listeners
+    return () => {
+      offerListener();
+      answerListener();
+    }
   }, [playerId]);
 
   function createPeer(initiator: boolean, peerId: string) {
     const peer = new Peer({
       initiator,
       trickle: false,
+      objectMode: true
     });
 
     peer.on("signal", async (signal) => {
@@ -74,17 +82,31 @@ export default function useWebRTC() {
       const data = await fetchApi(`/api/send-peer-signal?${params}`);
       const res = await data.json();
       if (res.error) {
-        console.error(res.error);
+        logOutOnError(res.error)
+        Object.keys(playerPeers.current).forEach((key) => {
+          playerPeers.current[key].destroy(new Error(`disconnected from ${key}`));
+        });
       }
     });
 
+    let isConnected = false;
+
     peer.on("connect", () => {
       peer.send(`connected to ${playerName}`);
+      isConnected = true;
     });
+
+    //removes peer if player is not connected within a certain time
+    setTimeout(() => {
+      if (!isConnected && playerPeers.current[peerId]) {
+        playerPeers.current[peerId].destroy(new Error(`${peerId} timed out`));
+        delete playerPeers.current[peerId];
+      }
+    }, 5000);
 
     peer.on("close", () => {
       if (process.env.NODE_ENV === "development") {
-        console.log(`disconnected from ${peerId}`);
+        console.log(`${peerId} left the game`);
       }
       delete playerPeers.current[peerId];
     });
@@ -93,15 +115,11 @@ export default function useWebRTC() {
       if (process.env.NODE_ENV === "development") {
         console.log(error.message);
       }
+      delete playerPeers.current[peerId];
     });
 
     peer.on("data", (data) => {
-      if (data instanceof Uint8Array) {
-        const decodedData = new TextDecoder().decode(data);
-        console.log(decodedData);
-      } else {
-        console.log(data);
-      }
+      console.log(data)
     });
 
     return peer;
--- apps/skraggle/src/hooks/useLogOutOnError.tsx
+++ apps/skraggle/src/hooks/useLogOutOnError.tsx
@@ -3,15 +3,22 @@ import { useToast } from "./use-toast";
 import type { MainMenuState } from "./usePanelUI";
 import { mainMenuState } from "@/store/store";
 import { resetClientSessionData } from "@/store/logInSlice";
+import { useUnityReactContext } from "@/app/_unity-player/UnityContext";
+import { PlayerPeers } from "@/app/_unity-player/useWebRTC";
 
 const defaultState: MainMenuState = {
   state: "Home",
   slideFrom: "left",
 };
 
-export default function useLogOutOnError() {
+export default function useLogOutOnError(disconnectPeers: boolean = true) {
   const dispatch = useDispatch();
   const { toast } = useToast();
+  let playerPeers: React.MutableRefObject<PlayerPeers>;
+
+  if (disconnectPeers) {
+    playerPeers = useUnityReactContext().playerPeers;
+  }
 
   const logOutOnError = (
     error: unknown,
@@ -28,6 +35,13 @@ export default function useLogOutOnError() {
 
     dispatch(resetClientSessionData());
     dispatch(mainMenuState.updateState(state));
+
+    //disconnect all webRTC peers
+    if (playerPeers) {
+      Object.keys(playerPeers.current).forEach((key) => {
+        playerPeers.current[key].destroy(new Error(`disconnected from ${key}`));
+      });
+    }
   };
 
   return { logOutOnError };
