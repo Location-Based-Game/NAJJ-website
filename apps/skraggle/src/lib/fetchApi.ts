491b357ef98b741884ad03df9749646619015fd3
MooKorea <69235304+MooKorea@users.noreply.github.com>
2024-10-27 21:26:36 -0500

    fix api path

--- apps/skraggle/src/lib/fetchApi.ts
+++ apps/skraggle/src/lib/fetchApi.ts
@@ -1,6 +1,6 @@
 export async function fetchApi(route: string) {
-  const isDev = process.env.NODE_ENV === "development" || "test";
-  const path = isDev ? "http://localhost:3000" : "https://skraggle.netlify.app";
+  const isDev = (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test");
+  const path = isDev ? "http://localhost:3000" : "";
   const data = await fetch(`${path}${route}`);
   const res = await data.json();
   if (res.error) {
