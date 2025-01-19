9e483809d77cec8c5d14bb7dedd1e720243a4ed3
MooKorea <69235304+MooKorea@users.noreply.github.com>
2025-01-18 21:10:17 -0600

    add cloud functions url

--- apps/skraggle/src/lib/fetchApi.ts
+++ apps/skraggle/src/lib/fetchApi.ts
@@ -1,7 +1,7 @@
 export async function fetchApi(route: string, payload: object = {}) {
   const isDev =
     process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test";
-  const path = isDev ? "http://localhost:5001/skraggle-2e19f/us-central1/" : "";
+  const path = isDev ? "http://localhost:5001/skraggle-2e19f/us-central1/" : "https://us-central1-skraggle-2e19f.cloudfunctions.net/";
   const data = await fetch(`${path}${route}`, {
     method: "POST",
     body: JSON.stringify(payload),
