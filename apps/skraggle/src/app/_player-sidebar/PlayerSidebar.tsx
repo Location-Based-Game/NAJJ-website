a064deae4a7c232fdc00b7d929f08126be003c12
MooKorea <69235304+MooKorea@users.noreply.github.com>
2025-01-21 19:18:55 -0600

    Player mobile sidebar

--- apps/skraggle/src/app/_player-sidebar/PlayerSidebar.tsx
+++ apps/skraggle/src/app/_player-sidebar/PlayerSidebar.tsx
@@ -6,12 +6,26 @@ import selectionStyles from "@styles/selectionGradient.module.css";
 import { cn } from "@/lib/tailwindUtils";
 import { PlayersData } from "@types";
 import { PeerStatuses } from "@/store/peerStatusSlice";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Users } from "lucide-react";
+import sidebarStyles from "./playerSidebar.module.css";
+import backgroundStyles from "@styles/main.module.css";
+import Noise from "@/components/Noise";
 
 export default function PlayerSidebar() {
   const players = useSelector((state: RootState) => state.players);
   const { currentTurn } = useSelector((state: RootState) => state.turnState);
   const peerStatuses = useSelector((state: RootState) => state.peerStatus);
   const { playerId } = useSelector((state: RootState) => state.logIn);
+
   return (
     <PlayerSidebarView
       players={players}
@@ -35,30 +49,60 @@ export function PlayerSidebarView({
   peerStatuses,
   playerId,
 }: PlayerSideBarView) {
-  return (
-    <div className="w-[14rem]">
+  const isMobile = useMediaQuery(1024);
+
+  const playerList = Object.entries(players).map(([id, data], index) => {
+    return (
+      <div
+        key={index}
+        className={cn(
+          "relative flex h-10 w-full items-center justify-end gap-2 overflow-hidden rounded-lg border-[1px] border-black/20 bg-black/30 px-4 text-lg",
+          currentTurn === data.turn && selectionStyles.selectionGradient2,
+        )}
+      >
+        <div className="grow font-semibold text-[#ffc56d]">{data.points}</div>
+        {data.name}
+        <ConnectionIcon
+          isMainPlayer={id === playerId}
+          name={data.name}
+          peerStatus={peerStatuses[id]}
+        />
+      </div>
+    );
+  });
+
+  return isMobile ? (
+    <Sheet>
+      {playerId && (
+        <SheetTrigger asChild>
+          <Button
+            variant="ghost"
+            className="fixed right-3 top-[6px] flex h-fit items-center gap-2 px-2 py-1 text-white transition-colors hover:bg-white/10 hover:text-white"
+          >
+            <span className="text-sm font-light opacity-70">Players</span>
+            <Users size={20} />
+          </Button>
+        </SheetTrigger>
+      )}
+      <SheetContent
+        side="right"
+        className={cn(
+          sidebarStyles.playerSidebarBorder,
+          backgroundStyles.mainBackground,
+          "w-[16rem] border-none text-white",
+        )}
+      >
+        <Noise />
+        <SheetHeader>
+          <SheetTitle className="text-white text-left mb-4 mt-[-1rem]">Players</SheetTitle>
+        </SheetHeader>
+        {playerList}
+      </SheetContent>
+    </Sheet>
+  ) : (
+    <div className="min-w-[14rem]">
       <div className="flex w-full flex-col gap-1 pl-4 pr-3 pt-12 text-white">
-        {Object.entries(players).map(([id, data], index) => {
-          return (
-            <div
-              key={index}
-              className={cn(
-                "relative flex h-10 w-full items-center justify-end gap-2 overflow-hidden rounded-lg border-[1px] border-black/20 bg-black/30 px-4 text-lg",
-                currentTurn === data.turn && selectionStyles.selectionGradient2,
-              )}
-            >
-              <div className="grow font-semibold text-[#ffc56d]">
-                {data.points}
-              </div>
-              {data.name}
-              <ConnectionIcon
-                isMainPlayer={id === playerId}
-                name={data.name}
-                peerStatus={peerStatuses[id]}
-              />
-            </div>
-          );
-        })}
+        {playerList}
       </div>
     </div>
   );
--- apps/skraggle/src/components/Noise.tsx
+++ apps/skraggle/src/components/Noise.tsx
@@ -1,8 +1,17 @@
-export default function Noise() {
+import { cn } from "@/lib/tailwindUtils";
+import { forwardRef } from "react";
+
+const Noise = forwardRef<SVGSVGElement, React.SVGAttributes<SVGSVGElement>>(
+  ({ className, ...props }, ref) => {
     return (
-        <svg
+      <svg
         xmlns="http://www.w3.org/2000/svg"
-        className="absolute h-full w-full mix-blend-multiply pointer-events-none"
+        className={cn(
+          "pointer-events-none absolute right-0 top-0 h-full w-full mix-blend-multiply",
+          className,
+        )}
+        ref={ref}
+        {...props}
       >
         <filter id="noiseFilter">
           <feTurbulence
@@ -12,8 +21,11 @@ export default function Noise() {
             stitchTiles="stitch"
           />
         </filter>
-
         <rect width="100%" height="100%" filter="url(#noiseFilter)" />
       </svg>
-    )
-}
+    );
+  },
+);
+
+Noise.displayName = "Noise";
+export default Noise;
