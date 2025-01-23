2c7ca6145bfac5c7e5d4d20c7abf8fa4a971f31f
MooKorea <69235304+MooKorea@users.noreply.github.com>
2025-01-23 13:01:27 -0600

    fetch no store

--- apps/skraggle/src/lib/fetchApi.ts
+++ apps/skraggle/src/lib/fetchApi.ts
@@ -3,6 +3,7 @@ export async function fetchApi(route: string, payload: object = {}) {
     process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test";
   const path = isDev ? "http://localhost:5001/skraggle-2e19f/us-central1/" : "https://us-central1-skraggle-2e19f.cloudfunctions.net/";
   const data = await fetch(`${path}${route}`, {
+    cache: "no-store",
     method: "POST",
     body: JSON.stringify(payload),
     credentials: "include",
