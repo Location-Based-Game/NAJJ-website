c7b91f8f9b551490b48829583f0f7ace0277b331
MooKorea <69235304+MooKorea@users.noreply.github.com>
2025-01-24 01:10:52 -0600

    update endpoints

--- apps/skraggle/functions/src/endpoints/sendPeerSignal.ts
+++ apps/skraggle/functions/src/endpoints/sendPeerSignal.ts
@@ -3,45 +3,40 @@ import { z } from "zod";
 import { deleteSession, getSessionData } from "../lib/sessionUtils";
 import { db } from "../lib/firebaseAdmin";
 import { SignalData } from "simple-peer";
+import { onAuthorizedRequest } from "../lib/onAuthorizedRequest";
 
 const sendPeerSignalSchema = z.object({
-    isInitiator: z.boolean(),
-    peerId: z.string().min(1),
-    signal: z.custom<SignalData>(),
-    name: z.string().min(1)
-  });
+  isInitiator: z.boolean(),
+  peerId: z.string().min(1),
+  signal: z.custom<SignalData>(),
+  name: z.string().min(1),
+});
 
-export const sendPeerSignal = onRequest({ cors: true }, async (request, response) => {
-    response.set("Access-Control-Allow-Credentials", "true");
-  
-    try {
-      const validatedData = sendPeerSignalSchema.safeParse(JSON.parse(request.body));
-  
-      if (!validatedData.success) {
-        response.send({ error: "Invalid Data!" });
-        return;
-      }
+export const sendPeerSignal = onAuthorizedRequest(async (request, response) => {
+  const validatedData = sendPeerSignalSchema.safeParse(
+    JSON.parse(request.body),
+  );
 
-      const { isInitiator, signal, peerId, name } = validatedData.data
-  
-      const { gameId, playerId } = await getSessionData(request);
-  
-      if (isInitiator) {
-        const offerRef = db.ref(
-          `activeGames/${gameId}/signaling/${peerId}/peer-offer`,
-        );
-        await offerRef.set({ signal, playerId, name });
-      } else {
-        const answerRef = db.ref(
-          `activeGames/${gameId}/signaling/${peerId}/peer-answer`,
-        );
-        await answerRef.set({ signal, playerId });
-      }
+  if (!validatedData.success) {
+    response.send({ error: "Invalid Data!" });
+    return;
+  }
 
-      response.send({ data: gameId });
-    } catch (error) {
-      deleteSession(response);
-      response.send({ error: `${error}` });
-    }
-  });
-  
+  const { isInitiator, signal, peerId, name } = validatedData.data;
+
+  const { gameId, playerId } = await getSessionData(request);
+
+  if (isInitiator) {
+    const offerRef = db.ref(
+      `activeGames/${gameId}/signaling/${peerId}/peer-offer`,
+    );
+    await offerRef.set({ signal, playerId, name });
+  } else {
+    const answerRef = db.ref(
+      `activeGames/${gameId}/signaling/${peerId}/peer-answer`,
+    );
+    await answerRef.set({ signal, playerId });
+  }
+
+  response.send({ data: gameId });
+});
